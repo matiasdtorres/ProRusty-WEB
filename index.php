d1e61914f53a008e915ca224ee485ebe86839101
matiasdtorres <matiasdanieltorres127@gmail.com>
2024-04-08 08:56:11 -0300

    [ADD] HisteriaServers LOGO
    [MOD] index para el team

--- index.php
+++ index.php
@@ -149,25 +149,25 @@
         <div class="cs_slider cs_slider_3">
           <div class="swiper-wrapper">
             <div class="swiper-slide">
-              <div class="text-center"><img src="assets/img/tech-startup/brand_1.svg" alt="Brand"></div>
+              <div class="text-center"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
             </div>
             <div class="swiper-slide">
-              <div class="text-center"><img src="assets/img/tech-startup/brand_2.svg" alt="Brand"></div>
+              <div class="text-center"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
             </div>
             <div class="swiper-slide">
-              <div class="text-center"><img src="assets/img/tech-startup/brand_3.svg" alt="Brand"></div>
+              <div class="text-center"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
             </div>
             <div class="swiper-slide">
-              <div class="text-center"><img src="assets/img/tech-startup/brand_4.svg" alt="Brand"></div>
+              <div class="text-center"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
             </div>
             <div class="swiper-slide">
-              <div class="text-center"><img src="assets/img/tech-startup/brand_5.svg" alt="Brand"></div>
+              <div class="text-center"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
             </div>
             <div class="swiper-slide">
-              <div class="text-center"><img src="assets/img/tech-startup/brand_6.svg" alt="Brand"></div>
+              <div class="text-center"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
             </div>
             <div class="swiper-slide">
-              <div class="text-center"><img src="assets/img/tech-startup/brand_1.svg" alt="Brand"></div>
+              <div class="text-center"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
             </div>
           </div>
         </div>
@@ -1009,6 +1009,17 @@
     <!-- End Pricing Section -->
     <!-- Start Team Section -->
     <section class="cs_p76_full_width">
+      <section class="cs_shape_animation_2">
+        <div class="cs_shape_1 position-absolute">
+          <svg width="57" height="41" viewBox="0 0 57 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path opacity="0.5" d="M55.1923 32.7347C52.209 37.0618 42.704 43.3879 40.3199 34.8097C39.0585 30.2359 42.6536 24.5172 43.1266 19.9687C43.8204 13.2821 39.973 5.41874 32.4359 6.05135C31.8886 6.10385 31.3452 6.19047 30.8086 6.31072C29.5308 4.21019 27.6918 2.5107 25.5002 1.40489C23.3085 0.299087 20.8519 -0.168816 18.4087 0.0542238C11.0797 0.743766 5.31489 7.39247 2.40097 13.6616C0.297443 17.9159 -0.0470809 22.831 1.44227 27.3386C2.02253 28.9897 4.52649 28.3381 4.07868 26.6111C2.33789 19.994 6.55742 12.4976 11.1238 7.99978C15.6903 3.50194 23.1517 2.572 26.8919 7.71511C22.3191 9.96086 18.2321 14.1614 15.6019 17.957C12.4483 22.5245 9.15598 31.3746 15.1794 35.2589C21.0135 39.0165 27.7181 36.5241 30.9852 30.8306C34.3217 24.9727 34.2461 16.9828 32.6377 10.6251C32.4379 9.84013 32.1764 9.07227 31.8556 8.32873C34.7948 7.65817 37.6078 8.80319 39.4747 11.7765C42.4517 16.502 40.9317 21.6009 39.5126 26.5289C38.1691 31.1785 36.6617 38.618 42.9185 40.5728C48.4057 42.2935 54.6246 38.6054 56.4853 33.2978C56.7691 32.507 55.6275 32.1085 55.1923 32.7347ZM27.8253 28.0345C26.6396 30.7231 24.5519 32.7537 21.5181 32.9751C16.3336 33.3547 15.3434 29.0909 16.4723 25.0739C18.1122 19.3298 23.2274 13.4845 28.1407 10.0684C28.1664 10.1199 28.1896 10.1727 28.2101 10.2266C30.2749 16.0071 30.1378 22.349 27.8253 28.0345Z" fill="#4F4747"/>
+          </svg>               
+        </div>
+        <div class="cs_shape_2 position-absolute">
+          <svg width="21" height="24" viewBox="0 0 21 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path opacity="0.5" d="M1.83883 0.992714L20.9086 13.6609L0.402728 23.8417L1.83883 0.992714Z" fill="#4F4747"/>
+            </svg>                       
+        </div>
       <div class="cs_height_143 cs_height_lg_75"></div>
       <div class="container">
         <div class="cs_section_heading cs_style_1 text-center">
@@ -1024,7 +1035,7 @@
             <div class="cs_team cs_style_1">
               <a class="cs_team_img cs_radius_5 overflow-hidden d-block"><img src="assets/img/studio-agency/team_2.jpg" alt="Team"></a>
               <div class="cs_team_info">
-                <h2 class="cs_fs_29"><a href="">Sukor</a></h2>
+                <h2 class="cs_fs_29"><a>Sukor</a></h2>
                 <p class="mb-0">Director & Developer</p>
               </div>
             </div>
@@ -1033,7 +1044,7 @@
             <div class="cs_team cs_style_1">
               <a class="cs_team_img cs_radius_5 overflow-hidden d-block"><img src="assets/img/studio-agency/team_1.jpg" alt="Team"></a>
               <div class="cs_team_info">
-                <h2 class="cs_fs_29"><a href="">Massi</a></h2>
+                <h2 class="cs_fs_29"><a>Massi</a></h2>
                 <p class="mb-0">Admin & Developer</p>
               </div>
             </div>
@@ -1042,7 +1053,7 @@
             <div class="cs_team cs_style_1">
               <a class="cs_team_img cs_radius_5 overflow-hidden d-block"><img src="assets/img/studio-agency/team_3.jpg" alt="Team"></a>
               <div class="cs_team_info">
-                <h2 class="cs_fs_29"><a href="">Clicon</a></h2>
+                <h2 class="cs_fs_29"><a>Clicon</a></h2>
                 <p class="mb-0">Admin & Developer</p>
               </div>
             </div>
@@ -1051,7 +1062,7 @@
             <div class="cs_team cs_style_1">
               <a class="cs_team_img cs_radius_5 overflow-hidden d-block"><img src="assets/img/studio-agency/team_4.jpg" alt="Team"></a>
               <div class="cs_team_info">
-                <h2 class="cs_fs_29"><a href="">Blammed</a></h2>
+                <h2 class="cs_fs_29"><a>Blammed</a></h2>
                 <p class="mb-0">Admin</p>
               </div>
             </div>
@@ -1059,6 +1070,7 @@
         </div>
         <div class="cs_pagination cs_style_1"></div>
       </div>
+      </section>
     </section>
     <!-- End Team Section -->
     <!-- Start Moving Text -->
@@ -1074,10 +1086,10 @@
     <div class="cs_height_84 cs_height_lg_70"></div>
     <div class="container">
       <div class="cs_brands cs_style_1 wow fadeInDown" data-wow-duration="0.8s" data-wow-delay="0.2s">
-        <div class="cs_brand"><img src="assets/img/marketing-agency/brand_1_dark.svg" alt="Brand"></div>
-        <div class="cs_brand"><img src="assets/img/marketing-agency/brand_2_dark.svg" alt="Brand"></div>
-        <div class="cs_brand"><img src="assets/img/marketing-agency/brand_3_dark.svg" alt="Brand"></div>
-        <div class="cs_brand"><img src="assets/img/marketing-agency/brand_4_dark.svg" alt="Brand"></div>
+        <div class="cs_brand"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
+        <div class="cs_brand"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
+        <div class="cs_brand"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
+        <div class="cs_brand"><img src="assets/img/tech-startup/HisteriaServers.svg" alt="Brand"></div>
       </div>
     </div>
     <div class="cs_height_135 cs_height_lg_80"></div>
